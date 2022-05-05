d23d8515f3b22a3aadd4688d5250d302b2c36882
hal0423 <theha2349@gmail.com>
2022-05-05 11:53:38 -0400

    first commit for HoYoLab Honkai Impact 3rd auto check in

--- src/background.js
+++ src/background.js
@@ -4,7 +4,10 @@ chrome.runtime.onStartup.addListener(function () {
 });
 async function onload() {
     var res = await fetch(
-        "https://hk4e-api-os.mihoyo.com/event/sol/sign?lang=vi-vn",
-        { method: "POST", body: JSON.stringify({ act_id: "e202102251931481" }) });
+        "https://api-os-takumi.mihoyo.com/event/mani/sign",
+        { 
+            method: "POST", 
+            body: JSON.stringify(
+                { act_id: "e202110291205111" }) });
     console.log(await res.text());
 }
